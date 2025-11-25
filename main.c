3fd0149410eba499c8787de05d0b3ab0e47e41e3
niladri <niladric006@gmail.com>
2025-11-26 00:04:52 +0530

    C-Program

---
+++ main.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main() {
+
+    //compound intrest calculator
+
+    double principal = 0.0;
+    double rate = 0.0;
+    int year = 0;
+    int timesCompunded = 0;
+    double total = 0.0;
+
+    printf("Enter ");
+
+    return 0;
+}
